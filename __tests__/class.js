3dc36530e3215d6d3cc8dfab29f57c5b09b5f792
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-11-13 16:51:56 -0800

    GET tests passing

--- __tests__/class.js
+++ __tests__/class.js
@@ -2,19 +2,18 @@ const server = require('../server');
 const request = require('supertest');
 const db = require('../data/config');
 
-beforeEach(async () => {
-    await db.seed.run();
-})
+// beforeEach(async () => {
+//     await db.seed.run();
+// })
 
-afterAll(async () => {
-    db.destroy();
+afterEach(async () => {
+    await db.destroy();
 })
 
 describe('Testing the classes endpoints', () => {
-    it('GETS the classes', async () => {
+    test('GETS the classes', async () => {
         jest.setTimeout(30000);
-        const res = await request(server).get("/classes")
+        const res = await request(server).get('/classes')
         expect(res.statusCode).toBe(200)
-        expect(res.type).toBe('application/json')
     })
 })
