d35384cfcaaa9a28e6c08e054d4cd6e4e405dc51
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2021-02-01 16:05:12 -0500

    added dynamic port for server deployment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const usersRouter = require("./data/users/users-router");
 const howtosRouter = require("./data/howtos/howtos-router");
@@ -7,10 +9,11 @@ server.use(express.json());
 server.use(usersRouter);
 server.use(howtosRouter);
 
+const port = process.env.PORT || 5000;
 server.get("/", (req,res) => {
     res.json({message: "Hello from the How-To Server!"})
 })
 
-server.listen(8080, () => {
-    console.log("server started");
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 })
